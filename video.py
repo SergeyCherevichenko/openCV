72bf36bcbdd1fb873a9cef0495cb2a6f2148cb98
SergeyCherevichenko <cherevichenkosn@gmail.com>
2023-10-11 18:00:57 +0500

    Работа с веб камерой распознание лиц

---
+++ video.py
@@ -0,0 +1,26 @@
+import cv2
+
+face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+# img = cv2.imread('11.jpg')
+# img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#
+# faces = face_cascades.detectMultiScale(img_gray)
+# for(x,y,w,h) in faces:
+#     cv2.rectangle(img,(x,y),(x+w,y + h),(255,0,0),2)
+#     cv2.imshow('img',img)
+#     cv2.waitKey(0)
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _,frame = cap.read()
+    cv2.imshow('Camera',frame)
+    img_gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_cascades.detectMultiScale(img_gray)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y + h),(255,0,0),2)
+    cv2.imshow('img', frame)
+
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
